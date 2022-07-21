6c5308856fa2ce27533f3bc6662b59db880fb4fa
james <james@gmail.com>
2022-07-21 21:51:34 +0700

    Initial Commit

---
+++ EvangelistTest/EvangelistTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvangelistTestApp.swift
+//  EvangelistTest
+//
+//  Created by James on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct EvangelistTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
